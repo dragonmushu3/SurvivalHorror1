2a862c40e3f2583091dfcdbc186bc6c73ac1275f
Damaschin Mushu <mushu.damaschin@gmail.com>
2022-11-22 01:36:03 +0100

    actually added the cpp class, can't add it from the editor apparently

---
+++ Source/SurvivalHorror1/MainCharacter.h
@@ -0,0 +1,41 @@
+// Mushu Damaschin. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class SURVIVALHORROR1_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Arrow Component for the Camera shoulder viewpoint location
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UArrowComponent *CameraRShoulderLocation;
+
+	// Arrow Component for the Camera Origin viewpoint location
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UArrowComponent *CameraOriginLocation;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UCameraComponent* CameraComp;
+
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
